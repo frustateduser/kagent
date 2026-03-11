470648db4e5ce9ec5f298e39ab4ba2fb0cbcc92b
Ishita mahajan <142918056+ishu1820@users.noreply.github.com>
2026-03-11 18:48:22 +0530

    Resolve merge conflicts and add file access functionality (#20)
    * multi-line input configured
    
    * resolve conflicts
    
    * file acces (read files) added

--- kagent/core/chat_loop.py
+++ kagent/core/chat_loop.py
@@ -6,9 +6,21 @@ from rich.progress import Progress, SpinnerColumn, TextColumn
 from rich.console import Console  #used for different color of user input
 from prompt_toolkit import PromptSession
 from prompt_toolkit.key_binding import KeyBindings
+#file reading
+from kagent.tools.fileaccess import FileAccess
+
 
 console = Console()
+
 kb = KeyBindings()
+
+#ctrl+enter will submit the message
+@kb.add("c-d")
+def _(event):
+    event.app.exit(result=event.app.current_buffer.text)
+    
+#create prompt session    
+session = PromptSession(key_bindings=kb)
 
 #ctrl+enter will submit the message
 @kb.add("c-d")
@@ -23,35 +35,41 @@ def start_chat():
     memory = ConversationMemory()
     # Create a model object to interact with the LLM
     model = OllamaModel()
+    file_tool = FileAccess(root_dir="kagent")
     chatLogger = ChatLogger() # initialize object for conversation logging
 
     console.print("[red]\nType 'exit' to quit[/red]\n")
     console.print("[red]Press 'ctrl+D' to send the message[/red]\n")
 
     while True:
-
-        console.print("[bold red]You:[/bold red] \n",end="")
-        
         #multi-line input
-        user_input = session.prompt(multiline=True)
+        user_input = session.prompt("[bold red]You:[/bold red] ",multiline=True)
+        
 
         if user_input.lower() in ["exit", "quit"]:
             break
-        
-         # Add the user's message to conversation memory
-        memory.add_user_message(user_input)
+        # log user message
+        chatLogger.log_user(user_input)
+        #read file
+        if user_input.lower().startswith("read file"):
 
-        #loader while ai is generating response
-        # Send the entire conversation history to the model
-        # The model will generate a response based on previous context
-        with Progress(SpinnerColumn(), TextColumn("[bold cyan]AI is thinking..."),
-                      transient=True,) as progress:
-            progress.add_task("thinking",total=None)
+            file_name = user_input.replace("read file", "").strip()
+
+            file_content = file_tool.read_file(file_name)
+            
+            console.print("\n[bold cyan]File content:[/bold cyan]\n")
+            console.print(file_content)
+            continue 
+            
+
+        with Progress(
+             SpinnerColumn(),
+             TextColumn("[bold cyan]AI is thinking..."),
+              transient=True,
+        ) as progress:
+
+            progress.add_task("thinking", total=None)
             response = model.generate(memory.get_history())
-        
-        if not response:
-            response = "I couldn't generate a response"
-
         # Store the AI response in the memory
         memory.add_ai_message(response)
         chatLogger.log_agent(response) # logs agent response
